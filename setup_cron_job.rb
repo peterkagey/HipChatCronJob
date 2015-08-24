a9b0bf8c94c475c493c0493a9564e50785cd2f9a
peterokagey <pkagey@sweetspotdiabetes.com>
2015-08-24 13:48:04 -0700

    Update setup_cron_job.rb

--- setup_cron_job.rb
+++ setup_cron_job.rb
@@ -6,7 +6,7 @@ class CronJob
   JSON_PATH   = "#{Dir.pwd}/mzemel.json"
   AUTH_TOKEN  = ENV['HIPCHAT_AUTH_TOKEN']
   # http://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
-  def self.append_job
+  def self.append_cron_job
     puts `(crontab -l ; echo "#{CRON_TIME} curl -X POST -d @#{JSON_PATH} #{MESSAGE_URL}?auth_token=#{AUTH_TOKEN} --header #{HEADER}") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -`
   end
 end
