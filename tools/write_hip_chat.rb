d41cdd7e2414007f3978dd5fca76aebc91bbf042
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-08-27 15:19:04 -0700

    Move and refactor write_hip_chat.rb.

--- tools/write_hip_chat.rb
+++ tools/write_hip_chat.rb
@@ -1,14 +1,12 @@
-class HipChat
-  COMMAND     = ""
-  USER_NAME   = "@MichaelZemel"
+require 'json'
+class WriteHipChat
+  USER_NAME   = ENV["HIPCHAT_RECIPIENT"] || "@MichaelZemel"
   MESSAGE_URL = "https://api.hipchat.com/v2/user/#{USER_NAME}/message"
   HEADER      = "--header 'Content-Type:application/json'"
-  JSON_PATH   = "#{Dir.pwd}/mzemel.json"
   AUTH_TOKEN  = "?auth_token=" + ENV['HIPCHAT_AUTH_TOKEN']
   # http://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
   def self.send(message)
-    puts `curl -X POST -d '{"message": "#{message}"}' #{MESSAGE_URL}#{AUTH_TOKEN} #{HEADER}`
+    puts jsonify = {"message" => "#{message}"}.to_json.inspect
+    puts`curl -X POST -d #{jsonify} #{MESSAGE_URL}#{AUTH_TOKEN} #{HEADER}`
   end
 end
-
-HipChat.send(ARGV.join(" "))
