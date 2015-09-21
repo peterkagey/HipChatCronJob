06f2ea4048e4c08afe7faf99661cf691e5913c17
Peter Kagey <pkagey@sweetspotdiabetes.com>
2015-09-21 14:55:46 -0700

    Don't cURL.

--- tools/write_hip_chat.rb
+++ tools/write_hip_chat.rb
@@ -1,12 +1,24 @@
+require 'net/http'
 require 'json'
+require 'openssl'
+
 class WriteHipChat
   USER_NAME   = ENV["HIPCHAT_RECIPIENT"] || "@MichaelZemel"
   MESSAGE_URL = "https://api.hipchat.com/v2/user/#{USER_NAME}/message"
   HEADER      = "--header 'Content-Type:application/json'"
   AUTH_TOKEN  = "?auth_token=" + ENV['HIPCHAT_AUTH_TOKEN']
   # http://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
+
   def self.send(message)
-    puts jsonify = {"message" => "#{message}"}.to_json.inspect
-    puts`curl -X POST -d #{jsonify} #{MESSAGE_URL}#{AUTH_TOKEN} #{HEADER}`
+    uri = URI.parse("#{MESSAGE_URL}#{AUTH_TOKEN}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.add_field('Content-Type', 'application/json')
+    request.body = {'message' => "#{message}"}.to_json
+    puts request.body
+    http.request(request)
   end
 end
