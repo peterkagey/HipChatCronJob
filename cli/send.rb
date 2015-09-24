cda4827680061e8f24dd687272371a7fa22ac794
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-09-24 16:54:37 -0700

    you should be able to specify who you want to message to on the command line

--- cli/send.rb
+++ cli/send.rb
@@ -1,2 +1,18 @@
 require_relative '../tools/write_hip_chat'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on('-n', '--username NAME', 'Specify a user name') { |v| options[:user_name] = v }
+
+end.parse!
+
+user_name = options[:user_name]
+
+if user_name
+  WriteHipChat.set_user_name(user_name)
+end
+
 WriteHipChat.send(ARGV.join(" "))
--- tools/write_hip_chat.rb
+++ tools/write_hip_chat.rb
@@ -3,14 +3,24 @@ require 'json'
 require 'openssl'
 
 class WriteHipChat
-  USER_NAME   = ENV["HIPCHAT_RECIPIENT"] || "@MichaelZemel"
-  MESSAGE_URL = "https://api.hipchat.com/v2/user/#{USER_NAME}/message"
   HEADER      = "--header 'Content-Type:application/json'"
   AUTH_TOKEN  = "?auth_token=" + ENV['HIPCHAT_AUTH_TOKEN']
   # http://stackoverflow.com/questions/610839/how-can-i-programmatically-create-a-new-cron-job
 
-  def self.send(message)
-    uri = URI.parse("#{MESSAGE_URL}#{AUTH_TOKEN}")
+  def self.set_user_name(user_name)
+    @user_name = user_name
+  end
+
+  def self.user_name
+     (@user_name || ENV["HIPCHAT_RECIPIENT"]) || "@MichaelZemel"
+  end
+
+  def self.message_url
+    "https://api.hipchat.com/v2/user/#{self.user_name}/message"
+  end
+
+  def self.send(message,user_name=nil)
+    uri = URI.parse("#{self.message_url}#{AUTH_TOKEN}")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
