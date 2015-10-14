9924814ffede2b8b545b743af8509735a51fac2c
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-10-13 20:46:07 -0700

    Fixed the opts banner

--- cli/send.rb
+++ cli/send.rb
@@ -3,7 +3,7 @@ require 'optparse'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
+  opts.banner = "Usage: cli/send.rb -n @jeffreybaird 'test' "
 
   opts.on('-n', '--username NAME', 'Specify a user name') { |v| options[:user_name] = v }
 
@@ -11,8 +11,6 @@ end.parse!
 
 user_name = options[:user_name]
 
-if user_name
-  WriteHipChat.set_user_name(user_name)
-end
+WriteHipChat.set_user_name(user_name) if user_name
 
 WriteHipChat.send(ARGV.join(" "))
